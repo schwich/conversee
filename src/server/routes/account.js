d9e1a59ee43fef58ee6a320c3add04087136d678
Jordan Schwichtenberg <jordan.schwichtenber@navy.mil>
2018-03-01 09:21:20 -0800

    refactor all db code into async/await pattern from callback pattern

--- src/server/routes/account.js
+++ src/server/routes/account.js
@@ -4,43 +4,48 @@ const db = require('../db');
 const mongo = require('../mongo');
 const passport = require('passport');
 
-router.get('/getUserHiddenPosts', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.get('/getUserHiddenPosts', passport.authenticate('jwt', { session: false }), async (req, res) => {
   // NOTE req.user contains JWT payload after passport.authenticate middleware runs
-  db.any(`
-          SELECT
-            posts.*
-          FROM
-            posts
-          INNER JOIN 
-            "user-hidden-posts" AS uhp
-          ON
-            uhp.user_id = $1
-          AND
-            uhp.post_id = posts.id
-          `, [req.user.id])
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => console.log(err));
+  try {
+    const data = await db.any(`
+    SELECT
+      posts.*
+    FROM
+      posts
+    INNER JOIN 
+      "user-hidden-posts" AS uhp
+    ON
+      uhp.user_id = $1
+    AND
+      uhp.post_id = posts.id
+    `, [req.user.id]);
+
+    res.json(data);
+  }
+  catch (err) {
+    console.log(err);
+  }
 })
 
-router.get('/getUserSavedPosts', passport.authenticate('jwt', { session: false }), (req, res) => {
-  db.any(`
-          SELECT
-            posts.*
-          FROM
-            posts
-          INNER JOIN 
-            "user-saved-posts" AS usp
-          ON
-            usp.user_id = $1
-          AND
-            usp.post_id = posts.id
-          `, [req.user.id])
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => console.log(err));
+router.get('/getUserSavedPosts', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+    const data = await db.any(`
+    SELECT
+      posts.*
+    FROM
+      posts
+    INNER JOIN 
+      "user-saved-posts" AS usp
+    ON
+      usp.user_id = $1
+    AND
+      usp.post_id = posts.id
+    `, [req.user.id]);
+    res.json(data);
+  } 
+  catch (err) {
+    console.log(err);
+  } 
 })
 
 module.exports = router;
