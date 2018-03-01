c3f6a2d434e1ed9fce23f482a3a95149c547b41c
Jordan Schwichtenberg <jordanschwichtenberg@gmail.com>
2018-02-28 22:12:48 -0800

    Make sure username is in the JWT payload

--- src/server/server.js
+++ src/server/server.js
@@ -59,7 +59,7 @@ const jwtOptions = {
 const strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
   console.log('payload received ', jwt_payload);
   // get user from DB
-  db.one('SELECT id FROM users WHERE id = $1', [jwt_payload.id])
+  db.one('SELECT id, username FROM users WHERE id = $1', [jwt_payload.id])
     .then(function (user) {
       if (user) {
         next(null, user);
